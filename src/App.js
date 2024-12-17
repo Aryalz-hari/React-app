9619551bde3aed1c436abc13c4b6263e590129a7
Hari Aryal <aryalhari059@gmail.com>
2024-12-17 23:23:44 +0545

    one

--- src/App.js
+++ src/App.js
@@ -10,7 +10,12 @@ function App() {
    title:"Window",
     date:new Date(2021,2,23),
     amount:200
-}];
+},{
+    title:"Health",
+    date:new Date(2021,2,23),
+    amount:267
+  },
+];
   return (
       <div>
         <h2>Let's Get started</h2>
@@ -20,6 +25,9 @@ function App() {
        <ExpenseItem title={expense[1].title}
         amount={expense[1].amount} 
         date={expense[1].date}></ExpenseItem>
+               <ExpenseItem title={expense[2].title}
+        amount={expense[2].amount} 
+        date={expense[2].date}></ExpenseItem>
       
 
 </div>
